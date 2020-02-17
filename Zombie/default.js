15357fbeaa8297471c95f3ea33e9130b93277747
Elenalalala <57182511+Elenalalala@users.noreply.github.com>
2020-02-17 18:43:13 -0500

    Add files via upload

---
+++ Zombie/default.js
@@ -0,0 +1,10 @@
+var status = document.getElementsById("daughter");
+var status1 = document.getElementsById("self");
+var text = document.getElementsByClassname("yesDaughter");
+var text1 = document.getElementsByClassname("noDaughter");
+if (status = true) {
+    text.style.display("none");
+}
+if (status1 = true){
+    text1.style.display("none");
+}
